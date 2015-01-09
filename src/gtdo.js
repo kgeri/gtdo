52cb2207702e00ffcaf777230b506b01a2765398
Gergely Kiss <kiss.gergely@gmail.com>
2015-01-09 20:48:36 +0100

    simple search support

--- src/gtdo.js
+++ src/gtdo.js
@@ -8,38 +8,70 @@ gtdo.common = {
   }
 }
 
+gtdo.SearchFilter = function() {
+
+  this.bind = function(inputSelector, view) {
+    d3.select(inputSelector).on("input", function(d) {
+      var cleanQuery = clean(this.value);
+      view.items().attr("visibility", function(d) {
+        return !cleanQuery || matches(clean(d.title), cleanQuery) ? "visible" : "hidden";
+      });
+    });
+    return this;
+  };
+
+  var clean = function(text) {
+    var words = text.toLowerCase().split(" ");
+    return words;
+  };
+
+  var matches = function(cleanText, cleanQuery) {
+    // TODO improve text matching here
+    for(var i=0; i < cleanText.length; i++) {
+      for(var j=0; j < cleanQuery.length; j++) {
+        if(cleanText[i].indexOf(cleanQuery[j]) > -1) return true;
+      }
+    }
+    return false;
+  };
+}
+
 gtdo.ListView = function() {
   var itemHeight = 100;
-  var bar = null;
+  var taskItems = undefined;
 
-  this.apply = function(containerSelector, data) {
+  this.bind = function(containerSelector, data) {
     var tasks = d3.select(containerSelector)
     .attr("width", "100%")
     .attr("height", "100%");
 
     gtdo.common.assignOrdinals(data);
 
-    bar = tasks.selectAll("g")
+    taskItems = tasks.selectAll("g")
     .data(data, function(d) { return d.ord; })
     .enter().append("g")
     .each(setPosition)
     .call(moveToNow);
 
-    bar.append("rect")
+    taskItems.append("rect")
     .attr("width", 200 )
     .attr("height", itemHeight-1);
 
-    bar.append("text")
+    taskItems.append("text")
     .attr("x", 10 )
     .attr("y", itemHeight / 2)
     .attr("dy", ".35em")
     .text(function(d) { return d.title });
 
-    bar.call(d3.behavior.drag()
+    taskItems.call(d3.behavior.drag()
     .on("dragstart", dragstart)
     .on("drag", dragVertical)
     .on("dragend", dragend));
+
+    return this;
   };
+
+  this.items = function() { return taskItems };
 
   var setPosition = function(d) {
     d.x = 0;
@@ -80,7 +112,7 @@ gtdo.ListView = function() {
       });
       gtdo.common.assignOrdinals(data);
 
-      bar.filter(function(di) { return di === d ? null : di })
+      taskItems.filter(function(di) { return di === d ? null : di })
       .each(setPosition)
       .call(moveToSmooth);
     } else {
